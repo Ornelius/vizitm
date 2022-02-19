a0db36aeb506c5b5fdd9f723674249ee250f1efc
Denis Chernyagin <d.chernyagin@gmail.com>
2022-02-19 13:24:37 +0400

    Закоммкентировал в main //'' = ..

--- backend/config/main.php
+++ backend/config/main.php
@@ -59,7 +59,7 @@ return [
                 //'<controller:\w+>/<id:\d+>' => '<controller>/<view>',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                '' => 'request/new', //site/index
+                //'' => 'request/new', //site/index
                 '<_a:login|logout>' => 'auth/<_a>',
             ],
         ],
