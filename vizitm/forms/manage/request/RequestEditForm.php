f99c259a70e77e5163cd5fa08a6599e23e96ef12
Denis Chernyagin <d.chernyagin@gmail.com>
2022-02-22 10:58:51 +0400

    Добавил редактирование заявки

--- vizitm/forms/manage/request/RequestEditForm.php
+++ vizitm/forms/manage/request/RequestEditForm.php
@@ -19,7 +19,7 @@ class RequestEditForm extends Model
     public ?int                 $room           = null;
     //public $importance                   ;
     //public $due_date                     ;
-    public Request $_request ;
+    //public Request $_request ;
 
     public function __construct(Request $request, $config = [])
     {
